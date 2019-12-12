e70af08d0814fb791fd23f68c368ac0bdef6a153
Vladislav Radonjic <vanjar@gmail.com>
2019-12-12 16:03:45 +0100

    Created two functions as requested.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions calculate matrix inverse, and cashe result for
+## future use
 
-## Write a short comment describing this function
+## Creates a list object with matrix, cashed inverse
+## and appropriate get/set methods
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## A function that checkes if there is a cashed inverse
+## matrix available.
+## Eiter returns cashed inverse if one exists, or calculates
+## the inverse and caches it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("Getting cashed data..")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
